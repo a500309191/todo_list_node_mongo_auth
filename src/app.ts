11c4f6c80feed0c2961e1a3133e1e1d0996771c9
a500309191 <a500309191@gmail.com>
2023-03-07 23:46:57 +0400

    have made main crud routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,19 @@
 import express, { Application, Request, Response, NextFunction }  from "express"
 import bodyParser from "body-parser"
 import { connectDB } from "./db/mongo"
-import { userModel, User, noteModel, Note } from "./db/schemas"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
+import {
+    userModel,
+    User,
+    noteModel,
+    Note,
+    Token,
+    tokenModel
+} from "./db/schemas"
+import e from "express"
+import { NODATA } from "dns"
+import { createDeflate } from "zlib"
 
 
 const {
@@ -28,20 +38,19 @@ const authenticateToken = (
     next: NextFunction
 ) => {
     try {
-        const authHeader = req.headers.authorization
-        const token = authHeader && authHeader.split(" ")[1]
+        const token = req.headers.authorization
         if (token == null) {
-            return res.status(401).json({ success: false, error: "unathorized" })
+            return res.status(401).json({ success: false, error: "Unathorized" })
         }
         const payload = jwt.verify(token, JWT_ACCESS)
         if (payload) {
             req.user = payload
             next()
         } else {
-            return res.status(400).json({ success: false, error: "no access rights" })
+            return res.status(400).json({ success: false, error: "No access rights" })
         }
     } catch (error) {
-        res.status(400).json({ error })
+        res.status(400).json(error)
     }
 
 }
@@ -53,20 +62,19 @@ app.post(
     async (req: Request, res: Response): Promise<Response> => {
         const name = req.body.name
         const password = req.body.password
-
-        if (!name || !password) {
+        if (name && password) {
+            const newUser: User = await userModel.create({
+                name,
+                password: bcrypt.hashSync(req.body.password, 10)
+            })
+            if (newUser) {
+                return res.status(201).json({ succes: true, name: newUser.name })
+            } else {
+                return res.status(500).json({ success: false, error: "Something went wrong"})
+            }
+        } else {
             return res.status(400).json({ success: false, error: "Send needed params" })
         }
-        const newUser: User = await userModel.create({
-            name,
-            password: bcrypt.hashSync(req.body.password, 10)
-        })
-        if (newUser) {
-            return res.status(201).json({ succes: true, name: newUser.name })
-        } else {
-            return res.status(500).json({ success: false, error: "Something went wrong"})
-        }
-
     }
 )
 
@@ -89,38 +97,88 @@ app.post(
             if (!bcrypt.compareSync(req.body.password, user.password)) {
                 return res.json({ success: false, error: "Wrong password" })
             } else {
-                const token = await jwt.sign({ name: user.name }, JWT_ACCESS)
-                return res.status(201).json({ token })
+                const token: Token = await tokenModel.create({
+                    accessToken: jwt.sign({ name: user.name }, JWT_ACCESS),
+                    user: user._id
+                })
+                return res.status(201).json({
+                    accessToken: token.accessToken,
+                    userId: token.user
+                })
             }
         }
     }
 )
 
 
+// create note
 app.post(
     "/notes",
     authenticateToken,
     async (req: Request, res: Response): Promise<Response> => {
-        try {
-            const newNote: Note = await noteModel.create({ body: req.body.body })
-            if (newNote) {
-                return res.status(201).json(newNote)
+        const user = await tokenModel.findOne({ accessToken: req.headers.authorization })
+        if (user) {
+            const body = req.body.body
+            if (body) {
+                const newNote: Note = await noteModel.create({ body, user })
+                if (newNote) {
+                    console.log(newNote)
+                    return res.status(201).json({ success: true, newNote })
+                } else {
+                    return res.status(500).json({ success: false, error: "Something went wrong"})
+                }
             } else {
-                return res.status(500).json({ success: false, error: "Something went wrong"})
+                return res.status(400).json({ success: false, error: "Send needed params" })
             }
-        } catch (error) {
-            return res.json({ success: false, error })
+        } else {
+            return res.status(401).json({ success: false, error: "No access rights" })
         }
     }
 )
 
-// app.get(
-//     "/notes",
-//     authenticateToken,
-//     async (req: Request, res: Response): Promise<Response> => {
 
-//     }
-// )
+// get notes
+app.get(
+    "/notes",
+    authenticateToken,
+    async (req: Request, res: Response): Promise<Response> => {
+        const userId = await tokenModel.findOne({ accessToken: req.headers.authorization })
+        if (userId) {
+            const notes = await noteModel.find({ user: userId })
+            return res.status(201).json({ notes })
+        } else {
+            return res.status(401).json({ success: false, error: "No access rights" })
+        }
+    }
+)
+
+
+// update notes
+app.put(
+    "/notes/:id",
+    authenticateToken,
+    async (req: Request, res: Response): Promise<Response> => {
+        const user = await tokenModel.findOne({ accessToken: req.headers.authorization })
+        if (user) {
+            const body = req.body.body
+            const isDone = req.body.isDone
+            const _id = req.params.id
+            if (body && isDone) {
+                await noteModel.updateOne({ _id }, { body, isDone, user })
+                    .catch(error => res.status(400).json({ error }))
+                return res.status(201).json({ succes: true, body, isDone })
+            } else {
+                return res.status(400).json({ success: false, error: "Send needed params" })
+            }
+        } else {
+            return res.status(401).json({ success: false, error: "No access rights" })
+        }
+    }
+)
+
+
+app.delete
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -9,6 +9,7 @@ import {
 } from '@typegoose/typegoose';
 
 
+@modelOptions({ schemaOptions: { timestamps: true, versionKey: false} })
 export class User {
   @prop({ unique: true, required: true })
   public name!: string;
@@ -16,20 +17,33 @@ export class User {
   @prop({ required: true, minLength: 8, maxLength: 100 })
   public password!: string;
 
-  @prop({ ref: () => Note })
-  public notes!: Ref<Note>[];
+  // @prop({ ref: () => Note })
+  // public notes!: Ref<Note>[];
 }
 
-
+@modelOptions({ schemaOptions: { timestamps: true, versionKey: false} })
 export class Note {
   @prop({ required: true, minLength: 10, maxLength: 255 })
   public body!: string
 
   @prop({ default: false })
   public isDone!: boolean
+
+  @prop({ ref: () => User, required: true })
+  public user!: Ref<User>
+}
+
+@modelOptions({ schemaOptions: { timestamps: true, versionKey: false} })
+export class Token {
+  @prop({ required: true })
+  public accessToken!: string
+
+  @prop({ ref: () => User, required: true })
+  public user!: Ref<User>
 }
 
 
 export const userModel = getModelForClass(User);
 export const noteModel = getModelForClass(Note);
+export const tokenModel = getModelForClass(Token);
 
